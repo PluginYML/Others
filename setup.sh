052e0d36a05a76ec7b9ac5584266938af9299e3a
Lennard B <pluginyml.developer@gmail.com>
2020-08-24 11:39:35 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-
-apt install ca-certificates apt-transport-https lsb-release gnupg curl nano unzip -y
-wget -q https://packages.sury.org/php/apt.gpg -O- | apt-key add -
-echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/php.list
-apt update
-apt install apache2 -y
-apt install php7.4 php7.4-cli php7.4-curl php7.4-gd php7.4-intl php7.4-json php7.4-mbstring php7.4-mysql php7.4-opcache php7.4-readline php7.4-xml php7.4-xsl php7.4-zip php7.4-bz2 libapache2-mod-php7.4 -y
-apt install mariadb-server mariadb-client -y
-mysql_secure_installation
 cd /usr/share
 wget https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.zip -O phpmyadmin.zip
 unzip phpmyadmin.zip
