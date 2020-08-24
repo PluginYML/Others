abcce8893b5dc28a348d0998bb9d6d7db0c2047c
Lennard B <pluginyml.developer@gmail.com>
2020-08-24 11:36:44 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+apt install ca-certificates apt-transport-https lsb-release gnupg curl nano unzip -y
+wget -q https://packages.sury.org/php/apt.gpg -O- | apt-key add -
+echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/php.list
+apt update
+apt install apache2 -y
+apt install php7.4 php7.4-cli php7.4-curl php7.4-gd php7.4-intl php7.4-json php7.4-mbstring php7.4-mysql php7.4-opcache php7.4-readline php7.4-xml php7.4-xsl php7.4-zip php7.4-bz2 libapache2-mod-php7.4 -y
+apt install mariadb-server mariadb-client -y
+mysql_secure_installation
+cd /usr/share
+wget https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.zip -O phpmyadmin.zip
+unzip phpmyadmin.zip
+rm phpmyadmin.zip
+mv phpMyAdmin-*-all-languages phpmyadmin
+mv phpMyAdmin-*-all-languages phpmyadmin
+
+echo "# phpMyAdmin Apache configuration
+
+Alias /phpmyadmin /usr/share/phpmyadmin
+
+<Directory /usr/share/phpmyadmin>
+    Options SymLinksIfOwnerMatch
+    DirectoryIndex index.php
+</Directory>
+
+# Disallow web access to directories that don't need it
+<Directory /usr/share/phpmyadmin/templates>
+    Require all denied
+</Directory>
+<Directory /usr/share/phpmyadmin/libraries>
+    Require all denied
+</Directory>
+<Directory /usr/share/phpmyadmin/setup/lib>
+    Require all denied
+</Directory>" >> /etc/apache2/conf-available/phpmyadmin.conf
+
+a2enconf phpmyadmin
+systemctl reload apache2
+mkdir /usr/share/phpmyadmin/tmp/
+chown -R www-data:www-data /usr/share/phpmyadmin/tmp/
